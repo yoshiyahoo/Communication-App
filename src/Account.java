5be64f9a2a5607caff61f9300738fb96e338501f
Anna Hernandez <anna605eso@yahoo.com>
2025-04-15 15:41:15 -0700

    Added code to Account class

--- src/Account.java
+++ src/Account.java
@@ -1,30 +1,42 @@
 public class Account {
     private Role role;
     private String name;
-    private static int counter;
+    private static int counter = 0;
     private int ID;
 
     public Account(Role role, String name) {
-
+    	this.role = role;
+    	this.name = name;
+    	this.ID = counter++;
     }
 
     public void setRole(Role role) {
+    	
+    	this.role = role;
 
     }
 
     public void setName(String name) {
+    	
+    	this.name = name;
 
     }
 
     public Role getRole() {
+    	
+    	return this.role;
 
     }
 
     public String getName() {
+    	
+    	return this.name;
 
     }
 
     public int getID() {
+    	
+    	return this.ID;
 
     }
 }
