ee3a450dd9af4241b3604b74f99337e0300be197
Mari-CSU <kpowell18@horizon.csueastbay.edu>
2025-04-17 15:19:05 -0700

    Added basic Login class

---
+++ src/Login.java
@@ -0,0 +1,20 @@
+public class Login 
+{
+
+    private String username;
+    private String password; 
+    public static void main(String[] args) throws Exception 
+    {
+       
+    }
+
+    public Login(String username, String password)
+    {
+        this.username = username;
+        this.password = password; 
+    }
+
+    public String getUsername() {return this.username;}
+    public String getPassword() {return this.password; }
+
+}
