73a172966ee153868098ca1bbaf655ece8d7e818
AdamHaise <adamhaise3260@gmail.com>
2025-04-17 16:11:08 -0700

    finished message class

--- src/Message.java
+++ src/Message.java
@@ -3,24 +3,41 @@ These are text messages that will be passed between the server and client applic
  */
 
 // Needs to implement Serializeable
-public class Message {
+import java.io.Serializable;
+import java.util.Date;
+
+public class Message implements Serializable {
     private String msg;
-    private int time; // Discussion? Redo a int with an actual time?
+    private Date time; // Discussion? Redo a int with an actual time?
     private String accountName;
+    private String chatname;
+    private int chatID;
 
-    public Message(String msg, int time, String accountName) {
-
+    public Message(String msg, String accountName, String chatname, int chatID) {
+    	this.msg = msg;
+    	this.time = new Date();
+    	this.accountName = accountName;
+    	this.chatname = chatname;
+    	this.chatID = chatID;
     }
 
     public String getMsg() {
-
+    	return this.msg;
     }
 
-    public int getTime() {
-
+    public Date getTime() {
+    	return this.time;
     }
 
     public String getAccountName() {
-
+    	return this.accountName;
     }
+
+	public String getChatname() {
+		return this.chatname;
+	}
+
+	public int getChatID() {
+		return this.chatID;
+	}
 }
