27c286d86235a72ae51375a52f6f79e35aef5e23
yoshiyahoo <josiahtroup@gmail.com>
2025-04-24 14:13:37 -0700

    improved chat, added test suite class

---
+++ src/ChatTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ChatTest {
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/TestSuite.java
@@ -0,0 +1,8 @@
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite; 
+
+@Suite
+@SelectClasses({ChatTest.class})
+public class TestSuite {
+	// Nothing needed in the body b/c you have test classes outside of it
+}
