fe007cd90765664b8382b44dee3e9fb50b7b6168
yoshiyahoo <josiahtroup@gmail.com>
2025-04-25 18:56:37 -0700

    Added requested methods adam talked about

--- src/ChatTest.java
+++ src/ChatTest.java
@@ -1,11 +1,26 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-class ChatTest {
+public class ChatTest {
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	private void AddMessages() {
+		Account[] users = new Account[] {
+			new Account(Role.EMPLOYEE, "Bill"),
+			new Account(Role.ADMINISTRATOR, "Michael"),
+			new Account(Role.ADMINISTRATOR, "Joe"),
+			new Account(Role.EMPLOYEE, "Karen"),
+		};
+		
+		//public Message(String msg, String accountName, String chatname, int chatID) {
+		Message[] msgs = new Message[] {
+			new Message("My Boss Sucks", "Karen", "KarenBill", 3)
+		};
+		
+		Chat chat = new Chat(users,msgs, "KarenBill");
+		
+		assertEquals(chat.getMsgHistory().length, users.length);
 	}
 
 }
