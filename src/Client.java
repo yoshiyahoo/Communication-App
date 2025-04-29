10d3b50112a54871fd8979c5fbe8161ddef912d8
Mari-CSU <kpowell18@horizon.csueastbay.edu>
2025-04-29 15:28:31 -0700

    Added Make Chat

--- src/Client.java
+++ src/Client.java
@@ -104,8 +104,20 @@ public class Client {
     	return temp;
     }
 
-    public void makeChat() {
+     public void makeChat() {
+        String testName;
+        Chat newChat = Chat(account, testName);
+        try (
+                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+                ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
 
+            // Receive response from server
+            Database dataBase = (Database) in.readObject();
+            dataBase.addChat(newChat);
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
 
